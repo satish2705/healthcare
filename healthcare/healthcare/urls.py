6d0777d37b9d6fe557fd6bf8734cfc541712ebe4
John Satish <37880100+satish2705@users.noreply.github.com>
2025-02-28 16:07:30 +0000

    new file:   healthcare/healthcare/__init__.py 	new file:   healthcare/healthcare/__pycache__/__init__.cpython-312.pyc 	new file:   healthcare/healthcare/__pycache__/settings.cpython-312.pyc 	new file:   healthcare/healthcare/asgi.py 	new file:   healthcare/healthcare/settings.py 	new file:   healthcare/healthcare/urls.py 	new file:   healthcare/healthcare/wsgi.py 	new file:   healthcare/manage.py 	new file:   healthcare/myapp/__init__.py 	new file:   healthcare/myapp/admin.py 	new file:   healthcare/myapp/apps.py 	new file:   healthcare/myapp/forms.py 	new file:   healthcare/myapp/migrations/__init__.py 	new file:   healthcare/myapp/models.py 	new file:   healthcare/myapp/static/myapp/css/bootstrap.min.css 	new file:   healthcare/myapp/static/myapp/css/styles.css 	new file:   healthcare/myapp/static/myapp/img/your-background-image.jpg 	new file:   healthcare/myapp/templates/myapp/cnn.html 	new file:   healthcare/myapp/templates/myapp/medication.html 	new file:   healthcare/myapp/templates/myapp/predict.html 	new file:   healthcare/myapp/templates/myapp/register.html 	new file:   healthcare/myapp/templates/myapp/signin.html 	new file:   healthcare/myapp/templates/myapp/upload_data.html 	new file:   healthcare/myapp/tests.py 	new file:   healthcare/myapp/views.py

---
+++ healthcare/healthcare/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for healthcare project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("register/", views.register, name="register"),
+    path("signin/", views.signin, name="signin"),
+    path("admin/upload_data/", views.upload_data, name="upload_data"),
+    path("admin/cnn/", views.cnn, name="cnn"),
+    path("user/predict/", views.predict, name="predict"),
+    path("user/medication/", views.medication, name="medication"),
+    ]
---
+++ healthcare/myapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from.forms import RegistrationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            form = RegistrationForm()
+        return render(request,'register.html', {'form': form})
+    
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request,'signin.html', {'error': 'Invalid credentials'})
+    return render(request,'signin.html')
+
+def upload_data(request):
+    # Code for uploading data goes here
+    return render(request, 'upload_data.html')
+
+def cnn(request):
+    # Code for training CNN goes here
+    return render(request, 'cnn.html')
+
+def predict(request):
+    # Code for making predictions goes here
+    return render(request, 'predict.html')
+
+def medication(request):
+    # Code for generating medication plan goes here
+    return render(request, 'medication.html')
