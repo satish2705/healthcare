6ea080cc5373f0f03821492fc12f177729a0283b
John Satish <37880100+satish2705@users.noreply.github.com>
2025-03-09 10:52:42 +0000

    modified:   healthcare/db.sqlite3 	deleted:    healthcare/firebase-debug.log 	modified:   healthcare/healthcare/__pycache__/urls.cpython-312.pyc 	modified:   healthcare/myapp/__pycache__/forms.cpython-312.pyc 	modified:   healthcare/myapp/forms.py 	modified:   healthcare/myapp/static/css/styles.css 	modified:   healthcare/myapp/templates/header.html 	modified:   healthcare/myapp/templates/register.html 	modified:   healthcare/myapp/templates/signin.html

--- healthcare/myapp/forms.py
+++ healthcare/myapp/forms.py
@@ -2,12 +2,34 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter Password'}),
+        label=""
+        )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 're-enter Password'}),
+        label=""
+        )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Username'}),
+        label=""
+        )
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email'}),
+        label=""
+        )
+    
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if self.errors.get(field_name):
+                field.widget.attrs['class'] += ' is-invalid'
 
     class Meta:
         model = User
         fields = ["username", "email", "password", "confirm_password"]
+        
 
         def clean(self):
             cleaned_data = super().clean()
